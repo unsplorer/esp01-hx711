1261806e5fc6fe9caceefb4700ceb4626f9a2a98
unsplorer <Mitchell_Way@outlook.com>
2022-11-05 12:40:00 -0500

    removed calibration

--- src/main.cpp
+++ src/main.cpp
@@ -133,9 +133,9 @@ void showWeight() {
     lcd.print("Filament Weight");
     lcd.setCursor(0, 1);
     lcd.printf("%ld grams", filament_remaining);
-    if (filament_remaining > 2000 || filament_remaining < -275) {
-      calibrateScale();
-    }
+    // if (filament_remaining > 2000 || filament_remaining < -275) {
+    //   calibrateScale();
+    // }
   } else {
     showError("Weigh Failed"); // scale failed, display error
   }
@@ -268,82 +268,17 @@ void setup() {
 #endif
 }
 
-void handleHTTP(WiFiClient &client) {
-  static unsigned long previousTime = millis();
-  const int timeoutTime = 5000;
-  String header = "";
-  String currentLine = "";
-  while (client.connected() && millis() - previousTime <= timeoutTime) {
-    char c = client.read();
-    header += c;
-    if (c == '\n') { // if the byte is a newline character
-      // if the current line is blank, you got two newline characters in a row.
-      // that's the end of the client HTTP request, so send a response:
-      if (currentLine.length() == 0) {
-        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-        // and a content-type so the client knows what's coming, then a blank
-        // line:
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-type:text/html");
-        client.println("Connection: close");
-        client.println();
-
-        // turns the GPIOs on and off
-        if (header.indexOf("GET /5/on") >= 0) {
-        }
-
-        // Display the HTML web page
-        client.println("<!DOCTYPE html><html>");
-        client.println("<head><meta name=\"viewport\" "
-                       "content=\"width=device-width, initial-scale=1\">");
-        client.println("<link rel=\"icon\" href=\"data:,\">");
-        // CSS to style the on/off buttons
-        // Feel free to change the background-color and font-size attributes to
-        // fit your preferences
-        client.println("<style>html { font-family: Helvetica; display: "
-                       "inline-block; margin: 0px auto; text-align: center;}");
-        client.println(".button { background-color: #195B6A; border: none; "
-                       "color: white; padding: 16px 40px;");
-        client.println("text-decoration: none; font-size: 30px; margin: 2px; "
-                       "cursor: pointer;}");
-        client.println(".button2 {background-color: #77878A;}</style></head>");
-
-        // Web Page Heading
-        client.println("<body><h1>ESP8266 Web Server</h1>");
-
-        client.println("</body></html>");
-
-        // The HTTP response ends with another blank line
-        client.println();
-        // Break out of the while loop
-        break;
-      } else { // if you got a newline, then clear currentLine
-        currentLine = "";
-      }
-    } else if (c != '\r') { // if you got anything else but a carriage return
-                            // character,
-      currentLine += c;     // add it to the end of the currentLine
-    }
-  }
-}
 
 
 void loop() {
-  // WiFiClient client = server.available();         // listen for clients
-  // if (client) {
-  //   lcd.clear();
-  //   lcd.print("client conn");
-  //   handleHTTP(client);
-  //   client.stop();
-  // }
   server.handleClient();
-
   ArduinoOTA.handle();
 
-#ifdef FILAMENT_SCALE
-  if (checkWeight.isReady()) {
-    checkWeight.run();
-  }
-#endif
+  #ifdef FILAMENT_SCALE
+    if (checkWeight.isReady()) {
+      checkWeight.run();
+    }
+  #endif
+
   yield();
 }
