57548cc036d9fde9f43cbe07a83572e6426a0de7
unsplorer <Mitchell_Way@outlook.com>
2022-12-22 11:40:36 -0600

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
 #include "LittleFS.h"
-#include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <ArduinoOTA.h>
+#include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <Wire.h>
 
 #define SSD1306
 // #define LCD1602
-#define FILAMENT_SCALE // enable FILAMENT_SCALE component
-#define SERVER
+// #define FILAMENT_SCALE // enable FILAMENT_SCALE component
+// #define SERVER
 // ESP01 build pinouts
 #ifdef ESP01
-  #define SDA 0x00
-  #define SCL 0x02
-  #define RX_PIN 0x03
-  #define TX_PIN 0x01
-  #define LOADCELL_DOUT_PIN TX_PIN
-  #define LOADCELL_SCK_PIN RX_PIN
+#define SDA 0x00
+#define SCL 0x02
+#define RX_PIN 0x03
+#define TX_PIN 0x01
+#define LOADCELL_DOUT_PIN TX_PIN
+#define LOADCELL_SCK_PIN RX_PIN
 #endif
 // nodemcu build pinouts
 #ifdef NODEMCUV2
-  #define LOADCELL_DOUT_PIN D5
-  #define LOADCELL_SCK_PIN D6
+#define LOADCELL_DOUT_PIN D5
+#define LOADCELL_SCK_PIN D6
 #endif
 // prototypes
 void loadConfig();
@@ -38,16 +38,16 @@ void setup();
 void updateWeb();
 
 #ifdef LCD1602
-  #include <LiquidCrystal_I2C.h>
-  LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 #endif
 #ifdef SSD1306
-  #include <Adafruit_SSD1306.h>
-  #define SCREEN_WIDTH    128
-  #define SCREEN_HEIGHT   64
-  #define OLED_RESET      -1
-  #define SCREEN_ADDRESS  0x3D
-  Adafruit_SSD1306 lcd(SCREEN_WIDTH,SCREEN_HEIGHT,&Wire, OLED_RESET);
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3D
+Adafruit_SSD1306 lcd(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #endif
 class Task {
 private:
@@ -71,201 +71,203 @@ public:
 };
 
 #ifdef FILAMENT_SCALE
-  #include <HX711.h>
-  #define EMPTY_SPOOL_WEIGHT 241
-  #define ROLLER_WEIGHT 46
-  HX711 scale; // create hx711 object
-  Task updateDisplayWeight = Task(0, 2500, &showWeight);
-  Task getScaleData = Task(0, 2500, &updateScale);
-  typedef struct scaleData {
-    float weight = 0;
-    float calibration= 0;
-    float offset = 0;
-    float filament_remaining = 0;
-    bool calFlag = false;
-    int knownWeight = 0;
-  } scaleData_t;
-  scaleData_t scale_data;
+#include <HX711.h>
+#define EMPTY_SPOOL_WEIGHT 241
+#define ROLLER_WEIGHT 46
+HX711 scale; // create hx711 object
+Task updateDisplayWeight = Task(0, 2500, &showWeight);
+Task getScaleData = Task(0, 2500, &updateScale);
+typedef struct scaleData {
+  float weight = 0;
+  float calibration = 0;
+  float offset = 0;
+  float filament_remaining = 0;
+  bool calFlag = false;
+  int knownWeight = 0;
+} scaleData_t;
+scaleData_t scale_data;
 
-  void updateScale() {
-    if (scale.is_ready()) {
-      scale_data.weight = (scale.get_units(2));
-      scale_data.filament_remaining = scale_data.weight - EMPTY_SPOOL_WEIGHT;
-      if (scale_data.filament_remaining < 0) {
-        scale_data.filament_remaining = 0;
-      }
-      scale_data.calibration = scale.get_scale();
-      scale_data.offset = scale.get_offset();
+void updateScale() {
+  if (scale.is_ready()) {
+    scale_data.weight = (scale.get_units(2));
+    scale_data.filament_remaining = scale_data.weight - EMPTY_SPOOL_WEIGHT;
+    if (scale_data.filament_remaining < 0) {
+      scale_data.filament_remaining = 0;
     }
+    scale_data.calibration = scale.get_scale();
+    scale_data.offset = scale.get_offset();
   }
+}
 
-  void saveConfig() {
-    StaticJsonDocument<64> doc;
-    doc["calibration"] = scale.get_scale();
-    doc["offset"] = scale.get_offset();
-    File configFile = LittleFS.open("/config.json", "w");
-    serializeJson(doc, configFile);
-    configFile.close();
+void saveConfig() {
+  StaticJsonDocument<64> doc;
+  doc["calibration"] = scale.get_scale();
+  doc["offset"] = scale.get_offset();
+  File configFile = LittleFS.open("/config.json", "w");
+  serializeJson(doc, configFile);
+  configFile.close();
+}
+
+void countdown(int millis) {
+  while (millis) {
+    lcd.setCursor(116, 63);
+    lcd.print(millis / 1000);
+    lcd.display();
+    delay(1000);
+    millis = millis - 1000;
   }
+}
 
-  void countdown(int millis) {
-    while (millis) {
-      lcd.setCursor(116, 63);
-      lcd.print(millis / 1000);
-      lcd.display();
-      delay(1000);
-      millis = millis - 1000;
-    }
-  }
-
-  void calibrateScale() {
-    if (scale_data.knownWeight == 0){
-      lcd.clearDisplay();
-      lcd.println("Cal Failed");
-      lcd.println("Weight UnKnown");
-      lcd.display();
-      scale_data.calFlag = false;
-      delay(4000);
-      // ESP.reset();
-      return;
-    }
+void calibrateScale() {
+  if (scale_data.knownWeight == 0) {
     lcd.clearDisplay();
-    lcd.print("Calbrating");
-    lcd.setCursor(0, 1);
-    lcd.print("Scale");
+    lcd.println("Cal Failed");
+    lcd.println("Weight UnKnown");
     lcd.display();
-    countdown(2000);
-    scale.set_scale();
-    scale.tare();
-    lcd.clearDisplay();
-    lcd.printf("Place %dg",scale_data.knownWeight);
-    lcd.setCursor(0, 1);
-    lcd.print("On Scale");
-    lcd.display();
-    countdown(5000);
-    scale_data.calibration = scale.get_units(2) / scale_data.knownWeight;
-    lcd.clearDisplay();
-    lcd.print("cal weight");
-    lcd.setCursor(0, 1);
-    lcd.printf("%f", scale_data.calibration);
-    lcd.display();
-    countdown(2000);
-    scale.set_scale(scale_data.calibration);
-    saveConfig();
-    lcd.clearDisplay();
-    lcd.print("Calibration");
-    lcd.setCursor(0, 1);
-    lcd.print("Saved");
-    lcd.display();
-    countdown(2000);
-    lcd.clearDisplay();
-    lcd.print("Resetting");
-    lcd.setCursor(0, 1);
-    lcd.print("Device");
     scale_data.calFlag = false;
-    ESP.reset();
+    delay(4000);
+    // ESP.reset();
+    return;
   }
+  lcd.clearDisplay();
+  lcd.print("Calbrating");
+  lcd.setCursor(0, 1);
+  lcd.print("Scale");
+  lcd.display();
+  countdown(2000);
+  scale.set_scale();
+  scale.tare();
+  lcd.clearDisplay();
+  lcd.printf("Place %dg", scale_data.knownWeight);
+  lcd.setCursor(0, 1);
+  lcd.print("On Scale");
+  lcd.display();
+  countdown(5000);
+  scale_data.calibration = scale.get_units(2) / scale_data.knownWeight;
+  lcd.clearDisplay();
+  lcd.print("cal weight");
+  lcd.setCursor(0, 1);
+  lcd.printf("%f", scale_data.calibration);
+  lcd.display();
+  countdown(2000);
+  scale.set_scale(scale_data.calibration);
+  saveConfig();
+  lcd.clearDisplay();
+  lcd.print("Calibration");
+  lcd.setCursor(0, 1);
+  lcd.print("Saved");
+  lcd.display();
+  countdown(2000);
+  lcd.clearDisplay();
+  lcd.print("Resetting");
+  lcd.setCursor(0, 1);
+  lcd.print("Device");
+  scale_data.calFlag = false;
+  ESP.reset();
+}
 
-  void loadConfig() {
-    File configFile = LittleFS.open("/config.json", "r");
-    if (!configFile) {
-      lcd.clearDisplay();
-      lcd.print("Failed to Load");
-      lcd.setCursor(0, 1);
-      lcd.print("Calibration");
-      lcd.display();
-      delay(4000);
-      return;
-    }
+void loadConfig() {
+  File configFile = LittleFS.open("/config.json", "r");
+  if (!configFile) {
+    lcd.clearDisplay();
+    lcd.print("Failed to Load");
+    lcd.setCursor(0, 1);
+    lcd.print("Calibration");
+    lcd.display();
+    delay(4000);
+    return;
+  }
+  StaticJsonDocument<64> doc;
+  deserializeJson(doc, configFile);
+  scale_data.calibration = doc["calibration"];
+  scale_data.offset = doc["offset"];
+  if (scale_data.offset) {
+    scale.set_offset(scale_data.offset);
+  }
+  if (scale_data.calibration) {
+    scale.set_scale(scale_data.calibration);
+  }
+  lcd.clearDisplay();
+  lcd.println("Loaded");
+  lcd.println("Calibration");
+  lcd.printf("calvalue%.2f", scale_data.calibration);
+  lcd.display();
+  countdown(4000);
+  configFile.close();
+}
+
+void showWeight() {
+  lcd.clearDisplay();
+  lcd.setCursor(0, 0);
+  lcd.println("Filament Weight");
+  lcd.printf("%.2f grams", scale_data.filament_remaining);
+  lcd.display();
+}
+#ifdef SERVER
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+Task sendEvents(0, 5000, &updateWeb);
+
+void startServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+  server.serveStatic("/", LittleFS, "/");
+  server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request) {
     StaticJsonDocument<64> doc;
-    deserializeJson(doc, configFile);
-    scale_data.calibration = doc["calibration"];
-    scale_data.offset= doc["offset"];
-    if (scale_data.offset) {
-      scale.set_offset(scale_data.offset);
+    doc["reading"] = scale_data.filament_remaining;
+    doc["offset"] = scale_data.offset;
+    String json;
+    serializeJson(doc, json);
+    request->send(200, "application/json", json);
+    json = String();
+  });
+  events.onConnect([](AsyncEventSourceClient *client) {
+    if (client->lastId()) {
     }
-    if (scale_data.calibration){
-      scale.set_scale(scale_data.calibration);
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  server.on("/tare", HTTP_GET,
+            [](AsyncWebServerRequest *request) { scale.tare(2); });
+  server.on("/calibrate", HTTP_GET, [](AsyncWebServerRequest *request) {
+    scale_data.calFlag = true;
+    String message;
+    if (request->hasParam("known_weight", false)) {
+      message = request->getParam("known_weight")->value();
     }
-    lcd.clearDisplay();
-    lcd.println("Loaded");
-    lcd.println("Calibration");
-    lcd.printf("calvalue%.2f", scale_data.calibration);
-    lcd.display();
-    countdown(4000);
-    configFile.close();
-  }
-
-  void showWeight() {
-    lcd.clearDisplay();
-    lcd.setCursor(0, 0);
-    lcd.println("Filament Weight");
-    lcd.printf("%.2f grams", scale_data.filament_remaining);
-    lcd.display();
-  }
-  #ifdef SERVER
-    AsyncWebServer server(80);
-    AsyncEventSource events("/events");
-
-    Task sendEvents(0, 5000, &updateWeb);
-
-    void startServer() {
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(LittleFS, "/index.html", "text/html");
-      });
-      server.serveStatic("/", LittleFS, "/");
-      server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request) {
-        StaticJsonDocument<64> doc;
-        doc["reading"] = scale_data.filament_remaining;
-        doc["offset"] = scale_data.offset;
-        String json;
-        serializeJson(doc, json);
-        request->send(200, "application/json", json);
-        json = String();
-      });
-      events.onConnect([](AsyncEventSourceClient *client) {
-        if (client->lastId()) {
-        }
-        client->send("hello!", NULL, millis(), 10000);
-      });
-      server.addHandler(&events);
-      server.on("/tare", HTTP_GET, [](AsyncWebServerRequest *request) {
-        scale.tare(2);
-      });
-      server.on("/calibrate", HTTP_GET, [](AsyncWebServerRequest *request) {
-        scale_data.calFlag = true;
-        String message;
-        if (request->hasParam("known_weight", false)){
-          message = request->getParam("known_weight")->value();
-        }
-        scale_data.knownWeight = atoi(message.c_str());
-      });
-      server.on("/offset", HTTP_GET, [](AsyncWebServerRequest *request) {
-        String message;
-        if (request->hasParam("offset", false)){
-          message = request->getParam("offset")->value();
-        }
-        scale_data.offset = atoi(message.c_str());
-        scale.set_offset(scale_data.offset);
-      });
-      server.begin();
+    scale_data.knownWeight = atoi(message.c_str());
+  });
+  server.on("/offset", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String message;
+    if (request->hasParam("offset", false)) {
+      message = request->getParam("offset")->value();
     }
+    scale_data.offset = atoi(message.c_str());
+    scale.set_offset(scale_data.offset);
+  });
+  server.begin();
+}
 
-    void updateWeb() {
-      StaticJsonDocument<64> doc;
-      doc["reading"] = scale_data.filament_remaining;
-      String json;
-      serializeJson(doc, json);
-      events.send("ping", NULL, millis());
-      events.send(json.c_str(), "new_readings", millis());
-      json = String();
-    }
-  #endif
+void updateWeb() {
+  StaticJsonDocument<64> doc;
+  doc["reading"] = scale_data.filament_remaining;
+  String json;
+  serializeJson(doc, json);
+  events.send("ping", NULL, millis());
+  events.send(json.c_str(), "new_readings", millis());
+  json = String();
+}
+#endif
 #endif
 
 void startOTA() {
   lcd.clearDisplay();
-  ArduinoOTA.onStart([]() { lcd.println("Start") ;lcd.display(); });
+  ArduinoOTA.onStart([]() {
+    lcd.println("Start");
+    lcd.display();
+  });
   ArduinoOTA.onEnd([]() {
     lcd.clearDisplay();
     lcd.println("Success");
@@ -274,7 +276,7 @@ void startOTA() {
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     lcd.clearDisplay();
     lcd.println("Updating");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.printf("Progress: %u%%", (progress / (total / 100)));
     lcd.display();
   });
@@ -282,7 +284,7 @@ void startOTA() {
     lcd.printf("Error[%u]: ", error);
     lcd.display();
     if (error == OTA_AUTH_ERROR)
-      lcd.println("Auth Failed"); 
+      lcd.println("Auth Failed");
     else if (error == OTA_BEGIN_ERROR)
       lcd.println("Begin Failed");
     else if (error == OTA_CONNECT_ERROR)
@@ -295,10 +297,11 @@ void startOTA() {
   ArduinoOTA.begin();
 }
 
-void initDisplay(){
-  if(lcd.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)){
+void initDisplay() {
+  if (lcd.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     lcd.display();
-    delay(2000);;
+    delay(2000);
+    ;
 
     // clearDisplay buffer
     lcd.clearDisplay();
@@ -315,7 +318,6 @@ void initDisplay(){
   }
 }
 
-
 void setup() {
   int retry = 0, config_done = 0;
   LittleFS.begin();
@@ -327,21 +329,21 @@ void setup() {
   lcd.clearDisplay();
   lcd.print("Connecting...");
   lcd.display();
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    if (retry++ >= 20){
+    if (retry++ >= 20) {
       lcd.clearDisplay();
       lcd.println("Connection timeout");
       lcd.println("RunSmartConfig");
       lcd.display();
       WiFi.beginSmartConfig();
       // forever loop: exit only when SmartConfig packets have been received
-      while (true){
+      while (true) {
         delay(500);
         lcd.clearDisplay();
         lcd.println("RunSmartConfig");
         lcd.display();
-        if (WiFi.smartConfigDone()){
+        if (WiFi.smartConfigDone()) {
           lcd.clearDisplay();
           lcd.println("Config Success");
           lcd.display();
@@ -349,7 +351,7 @@ void setup() {
           break; // exit from loop
         }
       }
-      if (config_done == 1){
+      if (config_done == 1) {
         break;
       }
     }
@@ -380,7 +382,7 @@ void loop() {
 #endif
 
 #ifdef FILAMENT_SCALE
-  if (scale_data.calFlag){
+  if (scale_data.calFlag) {
     server.end();
     calibrateScale();
     startServer();
