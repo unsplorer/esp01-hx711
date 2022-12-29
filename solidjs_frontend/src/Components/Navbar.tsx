2da9f600d6a39fb89b14204403f5b726dacc4c40
unsplorer <Mitchell_Way@outlook.com>
2022-12-29 12:39:10 -0600

    refactor

--- solidjs_frontend/src/Components/Navbar.tsx
+++ solidjs_frontend/src/Components/Navbar.tsx
@@ -3,15 +3,17 @@ import Navitem from "./Navitem";
 
 const Navbar: Component = () => {
   let settings = <Navitem name="Settings" class="button h-12 w-24 bg-zinc-700 text-white rounded-lg text-base font-medium" aria-current="page"/>
-
+  let navTitle = <h1 class='text-xl ml-6'>Fila-Scale</h1>
+  let logo = <img src="logo.png" class='h-16 w-16' />
+  
   return (
     <nav class="bg-zinc-800 text-white flex items-center justify-between">
       <div class='flex items-center'>
-        <img src="logo.png" class='h-16 w-16 filter hue-rotate-60 saturate-200' />
-        <h1 class='text-xl ml-6'>Fila-Scale</h1>
+        {logo}
+        {navTitle}
       </div>
       <div class='mr-6'>
-      {settings}
+        {settings}
       </div>
     </nav>
 
