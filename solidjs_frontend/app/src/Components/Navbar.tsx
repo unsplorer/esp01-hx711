55a656c564f6792a72048e63b98a61732813ca25
unsplorer <Mitchell_Way@outlook.com>
2023-04-07 20:26:14 -0500

    linting

--- solidjs_frontend/app/src/Components/Navbar.tsx
+++ solidjs_frontend/app/src/Components/Navbar.tsx
@@ -1,8 +1,11 @@
 import {Component} from "solid-js";
 import Navitem from "./Navitem";
 
+interface props{
+  toggleDark: () => void;   
+}
 
-const Navbar: Component = (props) => {  
+const Navbar: Component = (props: props) => {  
   let settings = <Navitem name="" class="button h-12 w-12 bg-zinc-700 text-white rounded-lg text-base font-medium"/>
   let navTitle = <h1 class='text-2xl ml-6'>Fila-Scale</h1>
   let logo = <img src="logo.png" class='h-16 w-16' />
@@ -13,7 +16,7 @@ const Navbar: Component = (props) => {
         {logo}
         {navTitle}
       </div>
-      <div class='mr-6' onclick={props.toggleDark}>
+      <div class='mr-6' onClick={props.toggleDark}>
         {settings}
       </div>
     </nav>
